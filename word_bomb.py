cfc55f529423e2ad5941520406bfe217130504ab
Dylan Groff <72639202+djwgroff@users.noreply.github.com>
2025-03-17 17:12:52 -0400

    Update word_bomb.py
    Add search command and fix add word command.

--- word_bomb.py
+++ word_bomb.py
@@ -3,7 +3,7 @@ from random import choice
 from time import sleep
 from pydirectinput import write, press
 
-def get_window_titles(user32):
+def get_window_titles(user32) -> list[str]:
     titles = []
 
     def enum_windows_callback(hwnd, lParam):
@@ -19,18 +19,18 @@ def get_window_titles(user32):
     user32.EnumWindows(EnumWindowsProc(enum_windows_callback), 0) # Enumerate all windows
     return titles
 
-def focus_window(user32, title):
+def focus_window(user32, title: str):
     hwnd = user32.FindWindowW(None, title)
     if hwnd:
         user32.SetForegroundWindow(hwnd) # Focus window
     else:
         print(f"Window '{title}' not found!")
 
-def load_words(words_file):
+def load_words(words_file: str) -> list[str]:
     with open(words_file, "r") as file:
         return [line.strip() for line in file]
 
-def add_word(words_file, word):
+def add_word(words_file: str, word: str) -> bool:
     with open(words_file, "r+") as file:
         content = file.read()
         if word in content:
@@ -39,7 +39,7 @@ def add_word(words_file, word):
         file.write(word + "\n" + content)
         return True
 
-def remove_word(words_file, word):
+def remove_word(words_file: str, word: str) -> bool:
     with open(words_file, "r+") as file:
         lines = file.readlines()
         if word + "\n" not in lines:
@@ -49,13 +49,15 @@ def remove_word(words_file, word):
         file.writelines(lines)
         return True
 
-def select_windows(window_titles, auto):
+def select_windows(window_titles: list[str], auto) -> (str):
     cmd_window = ""
     roblox_window = ""
     
     if auto:
         for title in window_titles:
-            if title.startswith("Command Prompt"):
+            if title.startswith("Command Prompt") and cmd_window == "": # Only select Command Prompt window if word_bomb.exe isn't found
+                cmd_window = title
+            elif title.endswith("word_bomb.exe"): 
                 cmd_window = title
             elif title.startswith("Roblox"):
                 roblox_window = title
@@ -65,7 +67,7 @@ def select_windows(window_titles, auto):
             print(f"{idx+1}. {title}")
         while True:
             try:
-                cmd_window = window_titles[int(input("Select Command Prompt window: "))-1]
+                cmd_window = window_titles[int(input("Select Terminal window: "))-1]
                 roblox_window = window_titles[int(input("Select Roblox window: "))-1]
             except (ValueError, IndexError):
                 print("Invalid input.")
@@ -74,7 +76,7 @@ def select_windows(window_titles, auto):
 
     return cmd_window, roblox_window
 
-def find_words(prompt, list, number):
+def find_words(prompt: str, list: list[str], number: int) -> list[str]:
     matches = []
     for word in list:
         if len(matches) >= number:
@@ -91,7 +93,7 @@ def main():
     word_list = load_words(filename)
     focus_terminal = True
     sample_size = 5
-    delay = 50
+    delay = 20
 
     print("Enter prompt. /settings for settings.")
 
@@ -99,18 +101,30 @@ def main():
         cmd = input("> ").split()
         if not cmd:
             continue
-        if cmd[0] == "/reset":
+        elif cmd[0] == "/s":
+            if len(cmd) > 1:
+                matches = find_words(cmd[1], reversed(word_list), int(cmd[2]) if len(cmd) > 2 and cmd[2].isdigit() else 3)
+                if not matches:
+                    print("No matches found.")
+                else:
+                    for match in matches:
+                        print(match)
+            else:
+                print("Enter a prompt to search.")
+        elif cmd[0] == "/reset":
             word_list = load_words(filename)
             print("Reset used words list.")
         elif cmd[0] == "/delay":
             if len(cmd) > 1 and cmd[1].isdigit():
-                print(f"{delay}ms -> {int(cmd[1])}ms") 
+                if (new := int(cmd[1])) > 3000:
+                    print("Delay cannot exceed 3000ms.")
+                    continue
+                print(f"{delay}ms -> {new}ms") 
                 delay = int(cmd[1])
             else:
                 print(f"{delay}ms")
         elif cmd[0] == "/windows":
             cmd_window, roblox_window = select_windows(window_titles, auto=False)
-            print(f"Selected windows:\nCommand Prompt - {cmd_window}\nRoblox - {roblox_window}")
         elif cmd[0] == "/focus":
             focus_terminal = not focus_terminal
             print(f"Focus terminal window after typing: {not focus_terminal} -> {focus_terminal}")
@@ -123,24 +137,26 @@ def main():
         elif cmd[0] == "/addword":
             if len(cmd) > 1:
                 if add_word(filename, cmd[1]):
-                    word_list = load_words(filename)
+                    word_list = [cmd[1]] + word_list
                     print(f"Added '{cmd[1]}' to word list.")
                 else:
                     print(f"'{cmd[1]}' already in word list.")
         elif cmd[0] == "/removeword":
             if len(cmd) > 1:
                 if remove_word(filename, cmd[1]):
-                    word_list = load_words(filename)
+                    if cmd[1] in word_list:
+                        word_list.remove(cmd[1])
                     print(f"Removed '{cmd[1]}' from word list.")
                 else:
                     print(f"'{cmd[1]}' not found in word list.")
         elif cmd[0] == "/quit":
             break
         elif cmd[0] == "/settings":
+            print("/s <prompt> <number> to search a prompt.")
             print("/reset to reset used words list.")
             print("/delay <ms> to change delay between key presses (<10ms not recommended).")
             print("/windows to change selected windows.")
-            print("/focus to change whether the terminal window is focused after typing.")
+            print("/focus to change whether the terminal window is automatically refocused.")
             print("/random <size> to change sample size for random word selection (how many matches to choose from).")
             print("/addword <word> to add a word to the word list text file.")
             print("/removeword <word> to delete a word from the word list text file.")
